f19a3cc129181b465a966bd10e012183165c6776
Naod Taddese <naodtds@gmail.com>
2024-06-06 22:15:15 +0300

    task 102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""annotation task"""
+
+from typing import Tuple, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """task for annotation"""
+    zoomed_in: Tuple[Any, ...] = tuple([
+        item for item in lst
+        for i in range(factor)
+    ])
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
