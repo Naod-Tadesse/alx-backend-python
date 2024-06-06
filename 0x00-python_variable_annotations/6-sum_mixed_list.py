e1efc2421d039662e38298f8f3d203b9d29058ee
Naod Taddese <naodtds@gmail.com>
2024-06-06 21:06:30 +0300

    mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""sum as float"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mixed_list: List[Union[int, float]]) -> float:
+    """mixed list sum"""
+    return sum(mixed_list)
