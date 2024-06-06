e0113c678e825c81e6a06cb22364c11ea3ae907a
Naod Taddese <naodtds@gmail.com>
2024-06-06 20:27:08 +0300

    task 0, 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+type annotated addition with python
+"""
+
+
+def add(number1: float, number2: float) -> float:
+    """add two numbers"""
+    return (number1 + number2)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+type annotated strings concatenator
+"""
+def concat(string1: str, string2: str) -> str:
+    return string1 + string2
+
