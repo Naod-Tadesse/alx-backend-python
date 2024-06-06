db408269fbb0f2a690c1d3b4188a2ad4cfe7df4c
Naod Taddese <naodtds@gmail.com>
2024-06-06 21:47:53 +0300

    fixes

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """duck typed annotations"""
 
-import typing
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
-        typing.Union[typing.Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """duck typed annotations"""
     if lst:
         return lst[0]
