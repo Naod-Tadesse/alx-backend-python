8c4d4b0ab7f77ac5f9153285c15110b9784dd923
Naod Taddese <naodtds@gmail.com>
2024-06-11 11:40:01 +0300

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+coroutine
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """function body"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,1 @@
+alx async  comprehension project
