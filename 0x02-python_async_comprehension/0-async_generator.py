2182795534a8c3ea4edf34528a2ab276ce385dd1
Naod Taddese <naodtds@gmail.com>
2024-06-11 18:56:30 +0300

    fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ coroutine
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """function body"""
     for _ in range(10):
         await asyncio.sleep(1)
